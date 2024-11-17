b900840f75bf503fd0d9ca30025d042d22ce4360
RodoxReis7 <rodoxreis@gmail.com>
2024-11-17 17:32:16 -0300

    DarkMode Testing

--- src/componentes/NavBar.tsx
+++ src/componentes/NavBar.tsx
@@ -128,7 +128,7 @@ const Navbar = () => {
                   onClick={toggleTheme}
                   className="px-4 py-2 bg-gray-300 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded"
                 >
-                  {theme === "dark" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+                  {theme === "dark" ? "🌙" : "☀️"}
                 </button>
               </li>
             </ul>
