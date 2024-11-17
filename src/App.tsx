21ebc96195eb9cd386bf6f5d6a227ea0af4ca5b0
RodoxReis7 <rodoxreis@gmail.com>
2024-11-17 15:52:27 -0300

    Upload Project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+import NavBar from './componentes/navbar'
+
+import Cta from './componentes/cta'
+
+import News from './componentes/news'
+
+import Pricing from './componentes/pricing'
+
+import Footer from './componentes/footer'
+
+
+function App() {
+ 
+  return (
+    <div>
+    <NavBar/>
+    <Cta/>
+    <News/>
+    <Pricing/>
+    <Footer/>
+    <br />
+      
+    
+    </div>
+  )
+}
+
+export default App
