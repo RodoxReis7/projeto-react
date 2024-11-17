ef4305259b2167e4ae8272c83e29d04a1f81eb81
RodoxReis7 <rodoxreis@gmail.com>
2024-11-17 18:00:49 -0300

    Modo Dark Mobile

--- src/componentes/NavBar.tsx
+++ src/componentes/NavBar.tsx
@@ -11,26 +11,29 @@ const Navbar = () => {
     return prefersDark ? "dark" : "light"; // Retorna dark ou light com base no sistema
   };
 
-  // Estado para armazenar o tema
+  // Estados para o tema e para o menu mobile
   const [theme, setTheme] = useState(getInitialTheme);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   // Atualiza o tema no HTML e no localStorage
   useEffect(() => {
     const root = document.documentElement;
-    // Verifica e aplica o tema na raiz (html)
     if (theme === "dark") {
       root.classList.add("dark");
     } else {
       root.classList.remove("dark");
     }
-
-    // Salva o tema atual no localStorage para persistir entre as sessões
     localStorage.setItem("theme", theme);
   }, [theme]);
 
   // Alterna entre os temas
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
+  // Alterna o menu mobile
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen((prev) => !prev);
   };
 
   return (
@@ -54,11 +57,10 @@ const Navbar = () => {
 
           {/* Botão hamburguer (responsivo) */}
           <button
-            data-collapse-toggle="navbar-default"
+            onClick={toggleMobileMenu}
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
+            aria-expanded={isMobileMenuOpen}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -79,7 +81,12 @@ const Navbar = () => {
           </button>
 
           {/* Navegação */}
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div
+            className={`${
+              isMobileMenuOpen ? "block" : "hidden"
+            } w-full md:block md:w-auto`}
+            id="navbar-default"
+          >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
